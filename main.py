7ef64c722ffd177550ec6fcc7304f634413ae518
Grangg <60051518+Grangg@users.noreply.github.com>
2022-02-19 19:13:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import random 
 
 
-def frequenceAniversaire(nbrTentative, nbrDate):
+def frequenceAniversaire(nbrTentative, nbrPersonnes):
     dates = []
     annivCommun = 0
     for i in range(nbrTentative):
         dates.clear()
-        for k in range(nbrDate):
+        for k in range(nbrPersonnes):
             dates.append(random.randint(1,365))
         if(len(dates)!= len(set(dates))):
            annivCommun = annivCommun + 1
     chance = annivCommun / nbrTentative
     return chance
-    
