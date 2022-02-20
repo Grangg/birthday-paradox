01616ab770cecf66aea62c1e96ab839da2bb36e8
Grangg <60051518+Grangg@users.noreply.github.com>
2022-02-20 12:28:15 +0100

    Add graph function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random 
+import random
+import matplotlib.pyplot as plt 
 
 
 def frequenceAniversaire(nbrTentative, nbrPersonnes):
@@ -12,3 +13,28 @@ def frequenceAniversaire(nbrTentative, nbrPersonnes):
            annivCommun = annivCommun + 1
     chance = annivCommun / nbrTentative
     return chance
+    
+
+def frequenceAniversaireGraph(nbrTentative, nbrPersonnesMax):
+    dates = []
+    annivCommun = 0
+    
+    xValues = []
+    yValues = []
+    
+    for v in range(nbrPersonnesMax):
+        annivCommun = 0
+        for i in range(nbrTentative):
+            dates.clear()
+            for k in range(v):
+                dates.append(random.randint(1,365))
+            if(len(dates)!= len(set(dates))):
+               annivCommun = annivCommun + 1
+        chance = (annivCommun / nbrTentative)*100
+        xValues.append(v)
+        yValues.append(chance)
+    plt.plot(xValues,yValues)
+    plt.xlabel('Nombre de personne')
+    plt.ylabel('Pourcentage de chance d\'avoir une date d\'anniversaire commune')
+    plt.axis([0, v, 0, 100])
+    plt.show()
